ee724af28f9f92b77868d7f92c36991b7dc88241
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-03-27 00:01:05 +0530

    :sparkles: added cors as well

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import quotesList from "./data/quotes";
-import { randomShuffle } from "./utils/shuffle";
+import { cors } from "hono/cors";
+import api from "./api";
 
 const app = new Hono();
 
 app.use("*", logger());
-
+app.use(
+  "/api/*",
+  cors({
+    origin: ["http://localhost:8000", "https://pranjal.me"],
+    allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true
+  })
+);
 app.get("/", (c) =>
   c.json({
     message: "Running happily ✨🌧️"
   })
 );
 
-app.get("/api/quotes/random", (c) => {
-  const shuffledQuotes = randomShuffle(quotesList);
-  return c.json({
-    count: 3,
-    quotes: shuffledQuotes.slice(0, 3)
-  });
-});
+app.route("/api", api);
 
 export default app;
